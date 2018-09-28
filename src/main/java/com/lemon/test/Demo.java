c560232f703e29dd5519f0b4b6853cbe411b8847
zhangsan <421586027@qq.com>
2018-09-28 14:57:11 +0800

    Initial commit

---
+++ src/main/java/com/lemon/test/Demo.java
@@ -0,0 +1,15 @@
+package com.lemon.test;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class Demo {
+    @Test
+    public void test1(){
+        ArrayList arrayList = new ArrayList();
+
+
+    }
+
+}
